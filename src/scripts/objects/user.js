01ef2629d421cde300cf63eba8c83538432bafd4
Livia Giro <lgmayrinck@gmail.com>
2023-12-08 17:27:08 -0300

    continuando o projeto javascript

---
+++ src/scripts/objects/user.js
@@ -0,0 +1,30 @@
+//import { getAndExtractEvents } from '../services/events.js';
+
+const user = {
+    avatarUrl: '',
+    name: '',
+    bio: '',
+    userName: '',
+    repositories: [],
+    events:[],
+    setInfo(gitHubUser){
+        this.avatarUrl = gitHubUser.avatar_url
+        this.name = gitHubUser.name
+        this.bio = gitHubUser.bio
+        this.userName = gitHubUser.login
+        this.followers = gitHubUser.followers
+        this.following = gitHubUser.following
+    },
+    setRepositories(repositories){
+        this.repositories = repositories
+        this.forks = repositories.forks_count
+        this.stars = repositories.stargazers_count
+        this.watchers = repositories.watchers_count
+        this.language = repositories.language
+    },
+   async setEvents(events) {
+        this.events = events
+        console.log(events)
+  }};
+
+export { user }
