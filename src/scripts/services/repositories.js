6bb6cab8d72c5016e2130955320d9c3d89c47312
Livia Giro <lgmayrinck@gmail.com>
2023-12-12 15:39:19 -0300

    changes

--- src/scripts/services/repositories.js
+++ src/scripts/services/repositories.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { baseUrl, repositoriesQuantity } from "../variables.js"
 
 async function getRepositories (userName) {
@@ -6,13 +5,4 @@ async function getRepositories (userName) {
     return await response.json()
 }
 
-=======
-import { baseUrl, repositoriesQuantity } from "../variables.js"
-
-async function getRepositories (userName) {
-    const response = await fetch (`${baseUrl}/${userName}/repos?per_page=${repositoriesQuantity}`)
-    return await response.json()
-}
-
->>>>>>> c38135b4cc186526bcc7c7a97ca34059daaa52c0
 export {getRepositories}
--- src/scripts/services/users.js
+++ src/scripts/services/users.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { baseUrl } from "../variables.js"
 
 async function getUser (userName) {
@@ -6,13 +5,4 @@ async function getUser (userName) {
     return await response.json()
 }
 
-=======
-import { baseUrl } from "../variables.js"
-
-async function getUser (userName) {
-    const response = await fetch (`${baseUrl}/${userName}`)
-    return await response.json()
-}
-
->>>>>>> c38135b4cc186526bcc7c7a97ca34059daaa52c0
 export { getUser }
