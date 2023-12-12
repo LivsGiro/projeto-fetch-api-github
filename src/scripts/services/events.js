843623a11996ebeebc5146aa9f150e797a25c75a
Livia Giro <lgmayrinck@gmail.com>
2023-12-12 15:39:26 -0300

    event

--- src/scripts/services/events.js
+++ src/scripts/services/events.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 import { baseUrl} from "../variables.js"
 
@@ -25,36 +24,4 @@ return {
    return extractedInfo.filter(info => info !== null);
  }
 
-=======
-
-import { baseUrl} from "../variables.js"
-
-// import { baseUrl, eventsQuantity } from "../variables.js";
-
-async function getAndExtractEvents(userName) {
-const response = await fetch (`${baseUrl}/${userName}/events?`)
-const events = await response.json();
-  
-// Filtra eventos para manter apenas CreateEvent e PushEvent
-const filteredEvents = events.filter(events => events.type === 'CreateEvent' || events.type === 'PushEvent');
-
-//   // Extrai informações relevantes dos eventos
-const extractedInfo = filteredEvents.map(events => {
-if (events.repo && events.payload && events.payload.commits && events.payload.commits.length > 0) {
-const repositoryName = events.repo.name;
-const commitMessage = events.payload.commits[0].message;
-
-return {
-      repoName: repositoryName,
-      message: commitMessage,
-      };
-    } else {
-      return null;
-     }
-   });
-
-   return extractedInfo.filter(info => info !== null);
- }
-
->>>>>>> c38135b4cc186526bcc7c7a97ca34059daaa52c0
  export { getAndExtractEvents };
