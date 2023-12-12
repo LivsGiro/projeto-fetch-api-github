a1399bdaa9f652316c81328449adbfb5112927bc
Livia Giro <lgmayrinck@gmail.com>
2023-12-12 15:40:02 -0300

    variables

--- src/scripts/variables.js
+++ src/scripts/variables.js
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
 const baseUrl = 'https://api.github.com/users'
 const repositoriesQuantity = 10
 
-=======
-const baseUrl = 'https://api.github.com/users'
-const repositoriesQuantity = 10
-
->>>>>>> c38135b4cc186526bcc7c7a97ca34059daaa52c0
 export { baseUrl, repositoriesQuantity }
